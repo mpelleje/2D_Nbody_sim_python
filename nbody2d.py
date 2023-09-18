bb5056c32af24df598269cdc99b4ce6e455fc7be
mac <mac@macs-MacBook-Pro.local>
2023-09-18 16:24:02 +0300

    Added a script for running 2dnbody simulations interactively from the commandline

---
+++ nbody2d.py
@@ -0,0 +1,89 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sheet_unfolding as su
+
+import time
+import argparse
+
+parser = argparse.ArgumentParser(
+                    prog='Nbody2D',
+                    description="Runs a small 2D N-body simulation. Example Usage: python nbody2d.py -n 128",
+                    epilog='')
+
+#parser.add_argument('-d', '--defaultsetup')
+parser.add_argument('-n', '--npart')
+parser.add_argument('-L', '--boxsize')
+parser.add_argument('-p', '--framepause')
+parser.add_argument('-s', '--drawsteps')
+parser.add_argument('-a', '--aspectratio')
+parser.add_argument('-w', '--width')
+parser.add_argument('--da')
+parser.add_argument('--aic')
+parser.add_argument('--afin')
+parser.add_argument('--pmgrid')
+parser.add_argument('-o', '--outdir')
+parser.add_argument('--no-overlay', action="store_true")
+
+args = parser.parse_args()
+
+c = {"npart": 128, "boxsize": 100., "drawsteps": 100, "framepause": 1e-4, "pmgrid":128, "outdir":None, "aic":0.05, "afin":1.0, "da":0.01, "aspectratio":1.5, "width": 10.0, "no_overlay": False}
+kwargs = vars(args)
+for key in kwargs:
+    if kwargs[key] is not None:
+        if type(c[key]) == int:
+            c[key] = int(kwargs[key])
+        elif type(c[key]) == float:
+            c[key] = float(kwargs[key])
+        else:
+            c[key] = kwargs[key]
+
+print("Using config", c)
+
+myic = su.sim.IC2DCosmo(ngrid = c["npart"], L=c["boxsize"], rs = 0.1, norm=2e7)
+
+sim = su.sim.CosmologicalSimulation2d(myic, aic=c["aic"], ngrid_pm=c["pmgrid"], verbose=0, da_max=c["da"], dafac_max=0.05)
+
+fig, ax = plt.subplots(1,1, figsize=(c["width"],c["width"]/c["aspectratio"]))
+#plt.tight_layout()
+fig.subplots_adjust(left=0., top=1.0, bottom=0., right=1.0)
+
+if c["aspectratio"] >= 1:
+    ax.set_xlim(0,sim.ics.L)
+    ax.set_ylim(0,sim.ics.L / c["aspectratio"])
+else:
+    ax.set_xlim(0,sim.ics.L * c["aspectratio"])
+    ax.set_ylim(0,sim.ics.L)
+    
+#ax.set_xlabel("x [Mpc]")
+#ax.set_ylabel("y [Mpc]")
+
+ai = np.linspace(c["aic"], c["afin"], c["drawsteps"])
+
+if c["outdir"] is not None:
+    import os
+    os.makedirs(c["outdir"], exist_ok=True)
+
+scatter = None
+for i in range(c["drawsteps"]):
+    sim.integrate_till(ai[i])
+    
+    pos = sim.pos % sim.ics.L
+    
+    if not c["no_overlay"]:
+        ax.set_title("a = %.2f" % sim.a, y=1.0, pad=-35, color="blue", fontsize=20)
+    
+    if scatter is not None:
+        scatter.remove()
+    
+    scatter = ax.scatter(pos[...,0].flat, pos[...,1].flat, marker=".", s=2, alpha=0.5, color="black")
+    plt.pause(max(c["framepause"], 1e-5))
+    
+    if c["outdir"] is not None:
+        fig.savefig("%s/image_%04d.png" % (c["outdir"], i))
+    
+if c["outdir"] is not None:
+    print("To create the video, paste the following line:")
+    print('ffmpeg -framerate 20 -i "%s/image_%s.png" -c:v libx264 -pix_fmt yuv420p %s.mp4' % (c["outdir"], "%04d", c["outdir"]))
+
+plt.show()
+
